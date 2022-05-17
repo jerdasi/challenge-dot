bf0419397183eab11754c3048a57b7ec01f1e6f3
jerdasi <jeremiadansil@gmail.com>
2022-05-17 18:07:59 +0700

    Update Handling Error Fetch Data

--- src/Components/Movies50.jsx
+++ src/Components/Movies50.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Card from './Card';
 
 export default function Movies50() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(null);
 
   useEffect(() => {
     axios.get('https://imdb-scraper.p.rapidapi.com/top250', {
@@ -12,14 +12,17 @@ export default function Movies50() {
         'X-RapidAPI-Key': '73bcf680f0msh406e672cab2e62bp1b4f6djsnfc58762d7c0d',
       },
     })
-      .then((res) => {
-        setMovies([...res.data.movies]);
-        console.log(res.data.movies);
-      });
+    .then((res) => {
+      setMovies([...res.data.movies]);
+      console.log(res.data.movies);
+    })
+    .catch((err) => {
+      alert("Terjadi kesalahan Server, Gagal Memuat Data")
+    })
   }, []);
   return (
     <div className="w-full h-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 text-left p-8">
-      {movies.slice(0, 50).map((item) => (
+      {movies === null? "No Data Displayed" : movies.slice(0, 50).map((item) => (
         <Card id={item.movieId} rating={item.Rating} title={item.Title} year={item.Year} rank={item.rank} />
       ))}
     </div>
